c38e43e6fefda19b29dd4ad3e863a1387f9aba99
Mattias Doevenspeck <mattias.doevenspeck@smals.be>
2026-07-13 11:03:08 +0200

    added missing import

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/java/spring/SpringCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/java/spring/SpringCodegenTest.java
@@ -56,6 +56,7 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
